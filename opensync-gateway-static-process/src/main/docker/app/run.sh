db827e7d43924e035f51add4a489bf490ca46a95
Rahul Sharma <rahulsharma@Rahuls-MacBook-Pro.local>
2020-03-10 15:24:18 -0400

    TW-144: Create Docker container for Opensync-gateway process
    This task deals with creating the docker container for Opensync-gateway process

--- opensync-gateway-static-process/src/main/docker/app/run.sh
+++ opensync-gateway-static-process/src/main/docker/app/run.sh
@@ -1,27 +1,28 @@
+#!/bin/bash
 PROFILES=" -Dspring.profiles.include=mqtt_receiver,ovsdb_redirector,ovsdb_manager"
 
 SSL_PROPS=" "
-SSL_PROPS+=" -Dssl.props=file:/app/certs/ssl.properties"
-SSL_PROPS+=" -Dwhizcontrol.httpClientConfig=file:/app/certs/httpClientConfig.json"
+SSL_PROPS+=" -Dssl.props=file:/opt/tip-wlan/certs/ssl.properties"
+SSL_PROPS+=" -Dwhizcontrol.httpClientConfig=file:/opt/tip-wlan/certs/httpClientConfig.json"
 
 CLIENT_MQTT_SSL_PROPS=" "
-CLIENT_MQTT_SSL_PROPS+=" -Djavax.net.ssl.keyStore=/app/certs/client_keystore.jks"
+CLIENT_MQTT_SSL_PROPS+=" -Djavax.net.ssl.keyStore=/opt/tip-wlan/certs/client_keystore.jks"
 CLIENT_MQTT_SSL_PROPS+=" -Djavax.net.ssl.keyStorePassword=mypassword"
-CLIENT_MQTT_SSL_PROPS+=" -Djavax.net.ssl.trustStore=/app/certs/truststore.jks"
+CLIENT_MQTT_SSL_PROPS+=" -Djavax.net.ssl.trustStore=/opt/tip-wlan/certs/truststore.jks"
 CLIENT_MQTT_SSL_PROPS+=" -Djavax.net.ssl.trustStorePassword=mypassword"
 CLIENT_MQTT_SSL_PROPS+=" -Dconnectus.mqttBroker.password=admin"
 
 OVSDB_PROPS=" "
-OVSDB_PROPS+=" -Dconnectus.ovsdb.managerAddr=________"
+OVSDB_PROPS+=" -Dconnectus.ovsdb.managerAddr=opensync-controller"
 OVSDB_PROPS+=" -Dconnectus.ovsdb.listenPort=6640 "
 OVSDB_PROPS+=" -Dconnectus.ovsdb.redirector.listenPort=6643"
 OVSDB_PROPS+=" -Dconnectus.ovsdb.timeoutSec=30"
-OVSDB_PROPS+=" -Dconnectus.ovsdb.trustStore=/app/certs/truststore.jks"
-OVSDB_PROPS+=" -Dconnectus.ovsdb.keyStore=/app/certs/server.pkcs12"
+OVSDB_PROPS+=" -Dconnectus.ovsdb.trustStore=/opt/tip-wlan/certs/truststore.jks"
+OVSDB_PROPS+=" -Dconnectus.ovsdb.keyStore=/opt/tip-wlan/certs/server.pkcs12"
 OVSDB_PROPS+=" -Dconnectus.ovsdb.configFileName=/app/opensync/config_2_ssids.json"
 
 MQTT_PROPS=" "
-MQTT_PROPS+=" -Dconnectus.mqttBroker.address=_________"
+MQTT_PROPS+=" -Dconnectus.mqttBroker.address=opensync-wifi-controller-opensync-mqtt-broker"
 MQTT_PROPS+=" -Dconnectus.mqttBroker.listenPort=1883"
 
 LOGGING_PROPS=" -Dlogging.config=file:/app/opensync/logback.xml"
@@ -31,5 +32,4 @@ RESTAPI_PROPS+=" -Dserver.port=443"
 
 export ALL_PROPS="$PROFILES $SSL_PROPS $CLIENT_MQTT_SSL_PROPS $OVSDB_PROPS $MQTT_PROPS $LOGGING_PROPS $RESTAPI_PROPS "
 
-sudo java $ALL_PROPS -jar opensync-gateway-static-process-0.0.1-SNAPSHOT.jar > stdout.out 2>&1 &
-
+java $ALL_PROPS -jar app.jar
