6550c656c9d08c4346b580af07f6cf83c53cc257
Lynn Shi <lynnshi@MacBook-Pro.local>
2020-06-19 11:07:00 -0400

    openSyncGW cloud docker NBI and SBI support

--- opensync-gateway-cloud-docker/src/main/docker/app/run.sh
+++ opensync-gateway-cloud-docker/src/main/docker/app/run.sh
@@ -12,8 +12,19 @@ CLIENT_MQTT_SSL_PROPS+=" -Djavax.net.ssl.trustStore=/opt/tip-wlan/certs/truststo
 CLIENT_MQTT_SSL_PROPS+=" -Djavax.net.ssl.trustStorePassword=mypassword"
 CLIENT_MQTT_SSL_PROPS+=" -Dconnectus.mqttBroker.password=admin"
 
+OVSDB_MANAGER_HOST=opensync-controller
+MQTT_BROKER_HOST=tip-wlan-opensync-mqtt-broke
+
+BACKEND_SERVER="${BACKEND_SERVER}"
+
+if [[ -n $BACKEND_SERVER ]]
+then
+  OVSDB_MANAGER_HOST=$BACKEND_SERVER
+  MQTT_BROKER_HOST=$BACKEND_SERVER 
+fi
+
 OVSDB_PROPS=" "
-OVSDB_PROPS+=" -Dconnectus.ovsdb.managerAddr=opensync-controller"
+OVSDB_PROPS+=" -Dconnectus.ovsdb.managerAddr=$OVSDB_MANAGER_HOST"
 OVSDB_PROPS+=" -Dconnectus.ovsdb.listenPort=6640 "
 OVSDB_PROPS+=" -Dconnectus.ovsdb.redirector.listenPort=6643"
 OVSDB_PROPS+=" -Dconnectus.ovsdb.timeoutSec=30"
@@ -22,7 +33,7 @@ OVSDB_PROPS+=" -Dconnectus.ovsdb.keyStore=/opt/tip-wlan/certs/server.pkcs12"
 OVSDB_PROPS+=" -Dconnectus.ovsdb.configFileName=/app/opensync/config_2_ssids.json"
 
 MQTT_PROPS=" "
-MQTT_PROPS+=" -Dconnectus.mqttBroker.address=tip-wlan-opensync-mqtt-broker"
+MQTT_PROPS+=" -Dconnectus.mqttBroker.address=$MQTT_BROKER_HOST"
 MQTT_PROPS+=" -Dconnectus.mqttBroker.listenPort=1883"
 
 LOGGING_PROPS=" -Dlogging.config=file:/app/opensync/logback.xml"
@@ -32,6 +43,28 @@ RESTAPI_PROPS+=" -Dserver.port=443"
 
 SPRING_EXTRA_PROPS=" --add-opens java.base/java.lang=ALL-UNNAMED"
 
-export ALL_PROPS="$PROFILES $SSL_PROPS $CLIENT_MQTT_SSL_PROPS $OVSDB_PROPS $MQTT_PROPS $LOGGING_PROPS $RESTAPI_PROPS $SPRING_EXTRA_PROPS"
+HOST_PROPS=" "
+if [[ -n $BACKEND_SERVER ]]
+then
+  echo Use specifed local host
+  HOST_PROPS+=" -Dtip.wlan.externalHostName=$BACKEND_SERVER"
+  HOST_PROPS+=" -Dtip.wlan.internalHostName=$BACKEND_SERVER"
+  HOST_PROPS+=" -Dtip.wlan.introspectTokenApi.host=${BACKEND_SERVER}:9091"
+  
+  HOST_URL=https://${BACKEND_SERVER}:9092
+  HOST_PROPS+=" -Dtip.wlan.cloudEventDispatcherBaseUrl=$HOST_URL"
+  HOST_PROPS+=" -Dtip.wlan.statusServiceBaseUrl=$HOST_URL"
+  HOST_PROPS+=" -Dtip.wlan.routingServiceBaseUrl=$HOST_URL"
+  HOST_PROPS+=" -Dtip.wlan.alarmServiceBaseUrl=$HOST_URL"
+  HOST_PROPS+=" -Dtip.wlan.customerServiceBaseUrl=$HOST_URL"
+  HOST_PROPS+=" -Dtip.wlan.locationServiceBaseUrl=$HOST_URL"
+  HOST_PROPS+=" -Dtip.wlan.equipmentServiceBaseUrl=$HOST_URL"
+  HOST_PROPS+=" -Dtip.wlan.profileServiceBaseUrl=$HOST_URL"
+  HOST_PROPS+=" -Dtip.wlan.clientServiceBaseUrl=$HOST_URL"
+fi
+#echo HOST_PROPS $HOST_PROPS
+
+
+export ALL_PROPS="$PROFILES $SSL_PROPS $CLIENT_MQTT_SSL_PROPS $OVSDB_PROPS $MQTT_PROPS $LOGGING_PROPS $RESTAPI_PROPS $SPRING_EXTRA_PROPS $HOST_PROPS"
 
 java $ALL_PROPS -jar app.jar
