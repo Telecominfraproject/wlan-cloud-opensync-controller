a3159ed34e5aa7fee700d06ef8f3681f1318ff30
Mike Hansen <mike.hansen@connectus.ai>
2020-11-03 16:57:15 -0500

    Wifi-996: Add GRE support to WifiInetConfigInfo

--- opensync-ext-cloud/src/main/java/com/telecominfraproject/wlan/opensync/external/integration/OpensyncExternalIntegrationCloud.java
+++ opensync-ext-cloud/src/main/java/com/telecominfraproject/wlan/opensync/external/integration/OpensyncExternalIntegrationCloud.java
@@ -828,6 +828,9 @@ public class OpensyncExternalIntegrationCloud implements OpensyncExternalIntegra
                 ret.setMetricsProfiles(
                         profileContainer.getChildrenOfType(equipmentConfig.getProfileId(), ProfileType.metrics));
 
+                ret.setGreTunnelProfiles(
+                        profileContainer.getChildrenOfType(equipmentConfig.getProfileId(), ProfileType.gre_tunnel));
+                
                 Set<Profile> radiusSet = new HashSet<>();
                 Set<Long> captiveProfileIds = new HashSet<>();
                 Set<Long> bonjourGatewayProfileIds = new HashSet<>();
